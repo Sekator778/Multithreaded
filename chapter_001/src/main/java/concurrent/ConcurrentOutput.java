4526e7ed7a9ba6127b83978f911078dcbf7ea553
Sekator778 <sekator778@gmail.com>
2020-05-19 22:38:16 +0300

    4

---
+++ chapter_001/src/main/java/concurrent/ConcurrentOutput.java
@@ -0,0 +1,11 @@
+package concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
