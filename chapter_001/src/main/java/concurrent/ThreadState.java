46b01c9fbb2eb5e26298b714e3af2743c75e7743
Sekator778 <sekator778@gmail.com>
2020-05-20 18:23:12 +0300

    1.1. Состояние нити.[229175#289859]

--- chapter_001/src/main/java/concurrent/ThreadState.java
+++ chapter_001/src/main/java/concurrent/ThreadState.java
@@ -1,19 +1,38 @@
 package concurrent;
 
 /**
+ * NEW - нить создана, но не запущена.
  *
+ * RUNNABLE - нить запущена и выполняется.
+ *
+ * BLOCKED - нить заблокирована.
+ *
+ * WAITING - нить ожидает уведомления.
+ *
+ * TIMED_WAITING - нить ожидает уведомление в течении определенного периода.
+ *
+ * TERMINATED - нить завершила работу.
  */
 
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {}
+                () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(first.getState());
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println("first run: " + first.getState());
+        System.out.println("second run: " + second.getState());
         first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
+        second.start();
+        while ((first.getState() != Thread.State.TERMINATED)) {
+            System.out.println("first state: " + first.getState());
         }
-        System.out.println(first.getState());
+        while ((second.getState() != Thread.State.TERMINATED)) {
+            System.out.println("second state: " + second.getState());
+        }
+        System.out.println("first terminated: " + first.getState());
+        System.out.println("second terminated: " + second.getState());
     }
 }
