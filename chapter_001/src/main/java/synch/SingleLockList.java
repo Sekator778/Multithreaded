3f436053ee4102a628554c4a346e3012e0d4a53c
Sekator778 <sekator778@gmail.com>
2020-05-22 19:01:59 +0300

    4. ThreadSafe динамический список [1105#289875]

--- chapter_001/src/main/java/synch/SingleLockList.java
+++ chapter_001/src/main/java/synch/SingleLockList.java
@@ -1,25 +1,49 @@
 package synch;
 
 import list.SimpleArrayList;
+import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.Iterator;
 
 @ThreadSafe
 public class SingleLockList<T> implements Iterable<T> {
-    private SimpleArrayList list = new SimpleArrayList();
-    
+    @GuardedBy("list")
+    private final SimpleArrayList<T> list = new SimpleArrayList<T>();
+
 
     public void add(T value) {
+        synchronized (list) {
+            list.add(value);
+        }
     }
 
-    public T get(int index) {
-        return null;
+    public synchronized T get(int index) {
+        synchronized (list) {
+            return list.get(index);
+        }
     }
 
+    private SimpleArrayList<T> copy() {
+        synchronized (list) {
+            SimpleArrayList<T> copy = new SimpleArrayList<T>();
+            for (T t : list
+            ) {
+                copy.add(t);
+            }
+            return copy;
+        }
+    }
 
+    /**
+     * Этот итератор будет работать в режиме fail-safe -
+     * все изменения после получения коллекции не будут отображаться в итераторе.
+     * @return итератор
+     */
     @Override
     public Iterator<T> iterator() {
-        return null;
+        synchronized (list) {
+            return copy().iterator();
+        }
     }
 }
--- chapter_001/src/test/synch/SingleLockListTest.java
+++ chapter_001/src/test/synch/SingleLockListTest.java
@@ -1,14 +1,14 @@
 package synch;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-@Ignore
+
 public class SingleLockListTest {
 
     @Test
@@ -23,5 +23,59 @@ public class SingleLockListTest {
         Set<Integer> rsl = new TreeSet<>();
         list.iterator().forEachRemaining(rsl::add);
         assertThat(rsl, is(Set.of(1, 2)));
+    }
+
+    @Test
+    public void get() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>();
+        Thread first = new Thread(
+                () -> {
+                    list.add(1);
+                    list.add(2);
+                    list.add(3);
+                });
+        Thread second = new Thread(
+                () -> {
+                    list.add(1);
+                    list.add(2);
+                    list.add(3);
+                });
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Thread rsl = new Thread(
+                () -> {
+                    int x = 0;
+                    for (int i = 0; i < 3; i++) {
+                        assertThat(list.get(i), is(++x));
+                    }
+                }
+        );
+        rsl.start();
+    }
+
+    @Test
+    public void iterator() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>();
+        Thread first = new Thread(
+                () -> {
+                    list.add(1);
+                    list.add(2);
+                    list.add(3);
+                });
+
+        Thread second = new Thread(
+                () -> {
+                    int x = 0;
+                    Iterator<Integer> iterator = list.iterator();
+                    while (iterator.hasNext()) {
+                        assertThat(iterator.next(), is(++x));
+                    }
+                });
+        first.start();
+        second.start();
+        first.join();
+        second.join();
     }
 }
