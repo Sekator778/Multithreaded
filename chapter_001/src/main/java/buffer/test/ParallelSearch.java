34f74e3924c1d77c2c7f2f6f585cc9e639d72fa3
Sekator778 <sekator778@gmail.com>
2020-05-24 18:06:56 +0300

    3. Junit тест для блокирующей очереди.[68589#289854]

---
+++ chapter_001/src/main/java/buffer/test/ParallelSearch.java
@@ -0,0 +1,37 @@
+package buffer.test;
+
+import blockingqueue.SimpleBlockingQueue;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class ParallelSearch {
+    public static void main(String[] args) {
+
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
