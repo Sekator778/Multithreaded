f2da4f1be446edcb85a29f64aa731dedb2118dbc
Sekator778 <sekator778@gmail.com>
2020-05-29 10:21:40 +0300

    правки мелкие

--- chapter_001/src/main/java/cache/CacheNonBlocking.java
+++ chapter_001/src/main/java/cache/CacheNonBlocking.java
@@ -6,16 +6,15 @@ import java.util.concurrent.ConcurrentHashMap;
  * кеш для хранение моделей. в кеше должны быть методы
  * add(Base model), update(Base model) delete(Base model),
  * <p>
- * 2. Для хранения данных в кеше использовать ConcurrentHashMap<Integer, Base>.
+ * Для хранения данных в кеше использовать ConcurrentHashMap<Integer, Base>.
  * <p>
  * В качестве ключа используйте int id. в качестве значения Base модель
  */
 
 public class CacheNonBlocking {
-    private final ConcurrentHashMap<Integer, Base> map;
+    private final ConcurrentHashMap<Integer, Base> map = new ConcurrentHashMap<>();
 
     public CacheNonBlocking() {
-        this.map = new ConcurrentHashMap<>();
     }
 
     public Base add(Base model) {
@@ -25,7 +24,7 @@ public class CacheNonBlocking {
     /**
      * public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
      * Если нам необходимо произвести какое-то действие со значением в Map, если оно там есть
-     * <p>
+     *
      * Если значение для указанного ключа присутствует, делаем тогда
      * попытку вычислить новое сопоставление с учетом ключа и его текущего сопоставленного значения.
      * Весь вызов метода выполняется атомарно.
